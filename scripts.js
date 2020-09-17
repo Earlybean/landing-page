e8a505d6e89df91ea6f50c821837da0cbabf0b1f
Blessing Philips <blessingadesina@gmail.com>
2020-09-17 13:20:41 +0100

    fix mobile issue

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@
 
 document.cookie = "fileDownload=true; path=/";
 $(document).ready(function () {
-    $('#download-btn').click(function(e) {
+    $('#download-btn-desktop').click(function(e) {
         e.preventDefault();  //stop the browser from following
-        var email = $("#email").val()
+        var email = $("#email-desktop").val()
         if(!email) {
             return
         }
@@ -24,8 +24,39 @@ $(document).ready(function () {
                 a.click();
                 a.remove();
                 window.URL.revokeObjectURL(url);
+                $("#mc-embedded-subscribe").trigger('click')
             }
         });
+
+        $("#mc-embedded-subscribe").trigger('click')
+    });
+
+    $('#download-btn-mobile').click(function(e) {
+        e.preventDefault();  //stop the browser from following
+        var email = $("#email-mobile").val()
+        if(!email) {
+            return
+        }
+        $("#mce-EMAIL").val(email)
+        $.ajax({
+            url: 'how_to_raise_money_smart_kids.pdf',
+            method: 'GET',
+            xhrFields: {
+                responseType: 'blob'
+            },
+            success: function (data) {
+                var a = document.createElement('a');
+                var url = window.URL.createObjectURL(data);
+                a.href = url;
+                a.download = 'how_to_raise_money_smart_kids.pdf.pdf';
+                document.body.append(a);
+                a.click();
+                a.remove();
+                window.URL.revokeObjectURL(url);
+                $("#mc-embedded-subscribe").trigger('click')
+            }
+        });
+
         $("#mc-embedded-subscribe").trigger('click')
     });
 })
